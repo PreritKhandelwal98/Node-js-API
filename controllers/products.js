9f0685a8221a2cf9698a7c851c5fe23bde80d507
Prerit Khandelwal <prerit.web@gmail.com>
2024-02-08 15:16:09 +0530

    search added according to fields

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,26 @@
 const Product = require('../models/product');
 
 const getAllDetails = async(req,resp)=>{
-    const myData = await Product.find({});
+
+    const {company,name} =req.query;
+    const queryObject={};
+
+    if(company){
+        queryObject.company=company;
+        //console.log(queryObject.company);
+    }
+    if(name){
+        queryObject.name={$regex:name,$options:"i"};
+    }
+    console.log(queryObject);
+
+    const myData = await Product.find({queryObject});
     resp.status(200).json({myData});
 };
 
 const getAllDetailsTesting = async(req,resp)=>{
-    resp.status(200).json({msg:"Get all testing details"});
+    const myData = await Product.find(req.query);
+    resp.status(200).json({myData});
 };
 
 module.exports = {getAllDetails,getAllDetailsTesting};
